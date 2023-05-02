1f990509f71fb8d061551afcc8a7a5ea119823a6
Simon Wacziarg <swacziarg@gmail.com>
2023-05-02 12:30:23 -0700

    comitf

--- out/myscript.js
+++ out/myscript.js
@@ -1,10 +1,16 @@
-const styleBtn = document.getElementById("style-btn");
-const pageStyle = document.getElementById("pagestyle");
+// get the button and stylesheets
+var styleBtn = document.getElementById("style-btn");
+var style1 = document.getElementById("pagestyle");
+var style2 = document.getElementById("pagestyle2");
 
-styleBtn.addEventListener("click", () => {
-  if (pageStyle.getAttribute("href") === "style1.css") {
-    pageStyle.setAttribute("href", "style2.css");
+// add event listener to button
+styleBtn.addEventListener("click", function() {
+  // check which stylesheet is currently active and switch to the other one
+  if (style1.getAttribute("href") === "style1.css") {
+    style1.setAttribute("href", "style2.css");
+    style2.setAttribute("href", "style1.css");
   } else {
-    pageStyle.setAttribute("href", "style1.css");
+    style1.setAttribute("href", "style1.css");
+    style2.setAttribute("href", "style2.css");
   }
 });
