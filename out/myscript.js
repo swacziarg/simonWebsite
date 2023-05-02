48655fc0ce7a17c80a9bb35a63d054c21190b78f
Simon Wacziarg <swacziarg@gmail.com>
2023-05-02 12:36:46 -0700

    ckima

--- out/myscript.js
+++ out/myscript.js
@@ -3,6 +3,9 @@ var styleBtn = document.getElementById("style-btn");
 var style1 = document.getElementById("pagestyle");
 var style2 = document.getElementById("pagestyle2");
 
+// set the default stylesheet
+style1.setAttribute("href", "style1.css");
+
 // add event listener to button
 styleBtn.addEventListener("click", function() {
   // check which stylesheet is currently active and switch to the other one
